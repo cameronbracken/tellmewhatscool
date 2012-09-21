84e492da1a5c3c838a932386e49feb25fef0a6b4
Cameron Bracken <cameron.bracken@gmail.com>
2012-09-20 20:25:01 -0500

    Use unicode in database

--- write_to_db.py
+++ write_to_db.py
@@ -7,26 +7,25 @@ import sys
 con = None
 data_file = 'review_data.csv'
 
-try:
 
-    f = open(data_file,'r')
-    lines = f.readlines()
-    
-    con = mdb.connect('localhost', 'bracken_tmwc', 
-        'iknowwhatscool', 'bracken_tmwc');
-    
-    
-    query = 'insert into ratings (site,score,album,artist,label,year,flag,url,reviewer,date_retrieved,date_released,date_reviewed) values ('
-    cur = con.cursor()
-    for i in reversed(len(lines)):
-        cur.execute(query + lines[i].strip() + ');')
-    
-except mdb.Error, e:
+f = open(data_file,'r')
+lines = f.readlines()
+
+con = mdb.connect('localhost', 'bracken_tmwc', 
+    'iknowwhatscool', 'bracken_tmwc',charset='utf8');
+
+
+query = 'insert into ratings (site,score,album,artist,label,year,flag,url,reviewer,date_retrieved,date_released,date_reviewed) values ('
+cur = con.cursor()
+for i in reversed(range(len(lines))):
+    try:
+	q = query + lines[i].strip() + ');'
+        cur.execute(q)
+    except mdb.Error, e:
+
+        print "Error %d: %s" % (e.args[0],e.args[1])
+        print q
+        #sys.exit(1)
   
-    print "Error %d: %s" % (e.args[0],e.args[1])
-    sys.exit(1)
-    
-finally:    
-        
-    if con:    
-        con.close()
+if con:
+    con.close()
